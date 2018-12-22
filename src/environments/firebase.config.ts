fa6fbdcea5b6672d7252fbc16617599913145e56
Michael <mike@solinta.co.za>
2018-12-22 11:51:22 +0200

    Remove API keys

--- src/environments/firebase.config.ts
+++
@@ -1,8 +0,0 @@
-export const firebase = {
-  apiKey: 'AIzaSyAiObNW0V7IilMQExpIaKIjnHRCYfYuPvg',
-  authDomain: 'ngnewtab.firebaseapp.com',
-  databaseURL: 'https://ngnewtab.firebaseio.com',
-  projectId: 'ngnewtab',
-  storageBucket: 'ngnewtab.appspot.com',
-  messagingSenderId: '394488680218'
-};
